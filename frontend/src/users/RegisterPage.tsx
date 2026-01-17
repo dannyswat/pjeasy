2fa84abfd4afb8acf37d335e13b27a56a566c181
Dannys Wat <dhlwat@live.com>
2026-01-17 20:11:07 +0800

    Fix warning

--- frontend/src/users/RegisterPage.tsx
+++ frontend/src/users/RegisterPage.tsx
@@ -27,7 +27,7 @@ export default function RegisterPage() {
   }
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-50 to-slate-100 flex items-center justify-center p-4">
+    <div className="min-h-screen bg-linear-to-br from-slate-50 to-slate-100 flex items-center justify-center p-4">
       <div className="max-w-md w-full">
         {/* Logo/Header */}
         <div className="text-center mb-6">
